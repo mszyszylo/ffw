aac282f1e3fa756f161909224137a78b6273f304
Michael Szyszylo <m.szyszylo@aol.com>
2023-11-20 10:06:27 +0100

    Initial Commit

---
+++ ffw_szyszylo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ffw_szyszylo
+//
+//  Created by Michael Szyszylo on 20.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ffw_szyszylo/ffw_szyszyloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ffw_szyszyloApp.swift
+//  ffw_szyszylo
+//
+//  Created by Michael Szyszylo on 20.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ffw_szyszyloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
